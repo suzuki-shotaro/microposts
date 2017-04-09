38c2ceb2677b8341312dc95e181e571cfd264322
shotaro <shotaro-suzuki@hotmail.co.jp>
2017-04-09 09:44:56 +0000

    edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :check_user, only: [:edit, :update]
+  before_action :set_params, only: [:show, :edit, :update]
+  
   def show #追加
    @user = User.find(params[:id])
   end
@@ -17,10 +20,30 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit
+  end 
+  
+  def update
+    if @user.update(user_params)
+      #保存に成功した場合はトップページへリダイレクト
+      redirect_to @user , notice: '基本情報を編集しました'
+    else
+      #保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   private
+  
+  def set_params
+    @user = User.find(params[:id])
+  end
   
   def user_params
     params.require(:user).permit(:name, :email, :password,:password_confirmation)
-    
+  end
+  
+  def check_user
+    @user = User.find(params[:id])
   end
 end
